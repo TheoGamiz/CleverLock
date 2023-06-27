422be31b4494624952b579764051628c59566e57
Theo Gmz <theogamiz31@gmail.com>
2023-06-27 22:22:29 +0200

    firestore

--- realtimetest.ino
+++ realtimetest.ino
@@ -1,357 +1,178 @@
+#include <ArduinoJson.h>
 #include <Arduino.h>
-#if defined(ESP32)
-  #include <WiFi.h>
+#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #endif
+
 #include <Firebase_ESP_Client.h>
 
-//Provide the token generation process info.
-#include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
-#include "addons/RTDBHelper.h"
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
 
-// Insert your network credentials
+/* 1. Define the WiFi credentials */
 #define WIFI_SSID "Bbox-7A11FB53"
 #define WIFI_PASSWORD "aKWsa2y61r6ED7VJ2d"
 
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyCUWxKi6okHafUKpHNj-vuYie_BS7e_uwM"
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyC8evfxnTsV25SvaeAMr6T0rcoAzHJOjZk"
 
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://projetserrure-86439-default-rtdb.europe-west1.firebasedatabase.app/" 
+/* 3. Define the project ID */
+#define FIREBASE_PROJECT_ID "smart-lock-4ee3f"
 
-//Define Firebase Data object
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "gamiz.theo@gmail.com"
+#define USER_PASSWORD "test123"
+
+// Define Firebase Data object
 FirebaseData fbdo;
 
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
-int intValue;
-int ledValue;
-int newFingerprint;
-float floatValue;
-bool signupOK = false;
+bool taskCompleted = false;
+int locker = 14;
 
+unsigned long dataMillis = 0;
 
-
-
-
-
-//fingerprint
-
-#include <Adafruit_Fingerprint.h>
-
-
-#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
-// For UNO and others without hardware serial, we must use software serial...
-// pin #2 is IN from sensor (GREEN wire)
-// pin #3 is OUT from arduino  (WHITE wire)
-// Set up the serial port to use softwareserial..
-SoftwareSerial mySerial(4, 5);
-
-#else
-// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
-// #0 is green wire, #1 is white
-#define mySerial Serial1
-
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+WiFiMulti multi;
 #endif
 
+void setup()
+{
 
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+    Serial.begin(115200);
+    pinMode(locker, OUTPUT);
 
-uint8_t id;
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    multi.addAP(WIFI_SSID, WIFI_PASSWORD);
+    multi.run();
+#else
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+#endif
 
+    Serial.print("Connecting to Wi-Fi");
+    unsigned long ms = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+        if (millis() - ms > 10000)
+            break;
+#endif
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
 
-void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(14, OUTPUT);
-  //digitalWrite(14, LOW);
-  Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-  /* Assign the api key (required) */
-  config.api_key = API_KEY;
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
 
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
+    /* Assign the user sign in credentials */
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
 
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
+    // The WiFi credentials are required for Pico W
+    // due to it does not have reconnect feature.
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    config.wifi.clearAP();
+    config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
 
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
+#if defined(ESP8266)
+    // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
+    fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
+#endif
 
+    // Limit the size of response payload to be collected in FirebaseData
+    fbdo.setResponseSize(2048);
 
+    Firebase.begin(&config, &auth);
 
+    Firebase.reconnectWiFi(true);
 
-
-  //fingerprint
-
-   delay(100);
-  Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
-
-  // set the data rate for the sensor serial port
-  finger.begin(57600);
-
-  if (finger.verifyPassword()) {
-    Serial.println("Found fingerprint sensor!");
-  } else {
-    Serial.println("Did not find fingerprint sensor :(");
-    while (1) { delay(1); }
-  }
-
-  Serial.println(F("Reading sensor parameters"));
-  finger.getParameters();
-  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
-  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
-  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
-  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
-  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
-  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
-  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
-
+    // You can use TCP KeepAlive in FirebaseData object and tracking the server connection status, please read this for detail.
+    // https://github.com/mobizt/Firebase-ESP-Client#about-firebasedata-object
+    // fbdo.keepAlive(5, 5, 1);
 }
 
+void loop()
+{
 
-uint8_t readnumber(void) {
-  uint8_t num = 0;
+    // Firebase.ready() should be called repeatedly to handle authentication tasks.
 
-  while (num == 0) {
-    while (! Serial.available());
-    num = Serial.parseInt();
-  }
-  return num;
-}
+    if (Firebase.ready() && (millis() - dataMillis > 200 || dataMillis == 0))
+    {
+        dataMillis = millis();
 
+        if (!taskCompleted)
+        {
+            taskCompleted = true;
 
-void loop() {
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 500 || sendDataPrevMillis == 0)) {
-    sendDataPrevMillis = millis();
-  
+            // For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create.ino
+            FirebaseJson content;
 
+           /* content.set("fields/Japan/mapValue/fields/time_zone/integerValue", "9");
+            content.set("fields/Japan/mapValue/fields/population/integerValue", "125570000");
 
+            content.set("fields/Belgium/mapValue/fields/time_zone/integerValue", "1");
+            content.set("fields/Belgium/mapValue/fields/population/integerValue", "11492641");
 
+            content.set("fields/Singapore/mapValue/fields/time_zone/integerValue", "8");
+            content.set("fields/Singapore/mapValue/fields/population/integerValue", "5703600");
 
+            // info is the collection id, countries is the document id in collection info.
+            String documentPath = "info/countries";
 
+            Serial.print("Create document... ");
 
+            if (Firebase.Firestore.createDocument(&fbdo, FIREBASE_PROJECT_ID, "" , documentPath.c_str(), content.raw()))
+                Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+            else
+                Serial.println(fbdo.errorReason());*/
+        }
 
+        String documentPath = "lock/XhCV3TOYYNEt8aR3jmzj";
+        String mask = "lock";
+       
 
+        if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), mask.c_str())) {
+          //Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
 
+          // Parse JSON payload
+          DynamicJsonDocument jsonDoc(1024);  // Adjust the size if necessary
+          DeserializationError error = deserializeJson(jsonDoc, fbdo.payload().c_str());
 
-//if led = 1
-    if (Firebase.RTDB.getInt(&fbdo, "/test/led")) {
-      if (fbdo.dataType() == "int") {
-        ledValue = fbdo.intData();
-        if(ledValue == 1){
-          digitalWrite(13, HIGH);
-          digitalWrite(14, HIGH);
+          // Check for parsing errors
+          if (error) {
+            Serial.print("JSON parsing error: ");
+            Serial.println(error.c_str());
+            return;
+          }
 
+          // Extract the population value
+          bool lock = jsonDoc["fields"]["lock"]["booleanValue"].as<bool>();
+          if (lock == 1){
+            digitalWrite(locker, HIGH);
+
+          }
+          else {
+            digitalWrite(locker, LOW);
+
+          }
+          Serial.println(lock);
         }
         else {
-          digitalWrite(13, LOW);
-          digitalWrite(14, LOW);
+          Serial.println(fbdo.errorReason());
         }
-        Serial.println(ledValue);
       }
-    }
-
-    else {
-      Serial.println(fbdo.errorReason());
-    }
-
-
-
-
- if (Firebase.RTDB.getInt(&fbdo, "/test/new_fingerprint")) {
-      if (fbdo.dataType() == "int") {
-        newFingerprint = fbdo.intData();
-        if(newFingerprint == 1){
-          getFingerprintEnroll();  
-        }
-        else {
-          
-        }
-        Serial.println(newFingerprint);
-      }
-    }
-
-    else {
-      Serial.println(fbdo.errorReason());
-    }
-
-
-
-
-
-
-
-
-
-
-  }
-}
-
-
-
-
-
-uint8_t getFingerprintEnroll() {
-
-  int p = -1;
-  Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
-  while (p != FINGERPRINT_OK) {
-    p = finger.getImage();
-    switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
-  }
-
-  // OK success!
-
-  p = finger.image2Tz(1);
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  Serial.println("Remove finger");
-  delay(2000);
-  p = 0;
-  while (p != FINGERPRINT_NOFINGER) {
-    p = finger.getImage();
-  }
-  Serial.print("ID "); Serial.println(id);
-  p = -1;
-  Serial.println("Place same finger again");
-  while (p != FINGERPRINT_OK) {
-    p = finger.getImage();
-    switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.print(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
-  }
-
-  // OK success!
-
-  p = finger.image2Tz(2);
-  switch (p) {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      return p;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      return p;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      return p;
-    default:
-      Serial.println("Unknown error");
-      return p;
-  }
-
-  // OK converted!
-  Serial.print("Creating model for #");  Serial.println(id);
-
-  p = finger.createModel();
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Prints matched!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
-    Serial.println("Fingerprints did not match");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-
-  Serial.print("ID "); Serial.println(id);
-  p = finger.storeModel(id);
-  if (p == FINGERPRINT_OK) {
-    Serial.println("Stored!");
-  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
-    return p;
-  } else if (p == FINGERPRINT_BADLOCATION) {
-    Serial.println("Could not store in that location");
-    return p;
-  } else if (p == FINGERPRINT_FLASHERR) {
-    Serial.println("Error writing to flash");
-    return p;
-  } else {
-    Serial.println("Unknown error");
-    return p;
-  }
-
-  return true;
 }
